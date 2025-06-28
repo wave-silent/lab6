2e9e0ba54a76496783366e5fd8b8a8d1aaac0ca4
wave-silent <danillogutov.74@mail.ru>
2025-06-28 12:35:35 +0000

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,22 @@
+import random
+
+def guess_the_number():
+    number = random.randint(1, 100)
+    attempts = 0
+
+    print("Угадай число от 1 до 100!")
+
+    while True:
+        guess = int(input("Ваша попытка: "))
+        attempts += 1
+
+        if guess < number:
+            print("Больше!")
+        elif guess > number:
+            print("Меньше!")
+        else:
+            print(f"Поздравляю! Вы угадали число за {attempts} попыток.")
+            break
+
+if name == "main":
+    guess_the_number()
